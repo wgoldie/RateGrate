e50642f7c55ce01ccd1a8d7143b37c2b4c0fea8f
William Goldie <will.goldie@mail.utoronto.ca>
2016-01-22 15:44:53 -0500

    Initial commit of project skeleton. Nothing except tooling implemented yet yet.

---
+++ TestApiServer/Controllers/RateTestController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using TestApiServer.Utilities;
+
+namespace TestApiServer.Controllers
+{
+    /// <summary>
+    /// A series of methods that will return the system tick time with various rate limits.
+    /// This is probably a bad endpoint to run in any "real life" scenario, 
+    /// it leaks internal system information that could potentially be a side channel to something.
+    /// </summary>
+    public class RateTestController : ApiController
+    {
+        /// <summary>
+        /// Unlimited api endpoint to serve as control variable.
+        /// </summary>
+        /// <returns>The current time.</returns>
+        /// 
+        [Route("ratetest")]
+        public int GetUnlimited()
+        {
+            return System.Environment.TickCount;
+        }
+
+        /// <summary>
+        /// Specify constant rate delay in milliseconds.
+        /// </summary>
+        /// <param name="ms">Constant rate delay in milliseconds.</param>
+        /// <returns>The current time if the request was ok.</returns>
+        [Route("ratetest/limited")]
+        [Throttle(Name = "TestThrottle", Message = "You must wait {n} seconds before accessing this url again.", Seconds = 5)]
+        public int GetLimited()
+        {
+            return System.Environment.TickCount;
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    /// <summary>
+    /// @todo: use actual testing framework.
+    /// Potentially can't use MSTest because the tests need to be run as part of project startup.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testBase = TestBase();
+            var testLimited = TestLimited();
+            Console.Write("Testing base endpoint...");
+            testBase.Wait();
+            Console.WriteLine(testBase.Result ? "Base passed." : "Base failed.");
+            Console.Write("Testing limited endpoint...");
+            testLimited.Wait();
+            Console.WriteLine(testLimited.Result ? "Limited passed." : "Limited failed.");
+            Console.ReadLine();
+        }
+
+        private const string BaseUri = "http://localhost:5555";
+
+        private static async Task<bool> TestBase()
+        {
+            using (var client = new HttpClient())
+            {
+                await client.GetAsync(new Uri(BaseUri + "/ratetest/5"));
+                var response = await client.GetAsync(new Uri(BaseUri + "/ratetest"));
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Base returned {0}", content);
+                int i;
+                return int.TryParse(content, out i);
+            }
+        }
+
+        private static async Task<bool> TestLimited()
+        {
+            using (var client = new HttpClient())
+            {
+                await client.GetAsync(new Uri(BaseUri + "/ratetest/limited"));
+                var response = await client.GetAsync(new Uri(BaseUri + "/ratetest/limited"));
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Limited returned {0}", content);
+                return content.Equals("You must wait 5 seconds before accessing this url again.");
+            }
+        }
+    }
+}
